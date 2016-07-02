c14b0f8268776ed01c9a0b40c91c90a2d5ae93f1
Supasate Choochaisri <supasate.c@gmail.com>
2016-07-03 00:29:07 +0700

    Init repo with 'feathers generate'

---
+++ src/services/authentication/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const authentication = require('feathers-authentication');
+
+const FacebookStrategy = require('passport-facebook').Strategy;
+const FacebookTokenStrategy = require('passport-facebook-token');
+const GithubStrategy = require('passport-github').Strategy;
+const GithubTokenStrategy = require('passport-github-token');
+
+module.exports = function() {
+  const app = this;
+
+  let config = app.get('auth');
+  
+  config.facebook.strategy = FacebookStrategy;
+  config.facebook.tokenStrategy = FacebookTokenStrategy;
+  config.github.strategy = GithubStrategy;
+  config.github.tokenStrategy = GithubTokenStrategy;
+
+  app.set('auth', config);
+  app.configure(authentication(config));
+};
