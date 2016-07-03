b0bd86ff336f8ebf37b2344bd37c35e7161c1f02
Supasate Choochaisri <supasate.c@gmail.com>
2016-07-03 09:27:18 +0700

    Require authentication before using messages service

--- src/services/messages/hooks/index.js
+++ src/services/messages/hooks/index.js
@@ -3,9 +3,14 @@
 const globalHooks = require('../../../hooks');
 const hooks = require('feathers-hooks');
 
+const auth = require('feathers-authentication').hooks;
 
 exports.before = {
-  all: [],
+  all: [
+    auth.verifyToken(),
+    auth.populateUser(),
+    auth.restrictToAuthenticated()
+  ],
   find: [],
   get: [],
   create: [],
