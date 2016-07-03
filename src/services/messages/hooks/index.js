bbc204ba7b047f9df98437a563c48e6e9c6be4b6
Supasate Choochaisri <supasate.c@gmail.com>
2016-07-03 10:14:54 +0700

    Use populate hook to populate sender

--- src/services/messages/hooks/index.js
+++ src/services/messages/hooks/index.js
@@ -1,11 +1,14 @@
 'use strict';
 
-const process = require('./process');
-
-const globalHooks = require('../../../hooks');
+const auth = require('feathers-authentication').hooks;
 const hooks = require('feathers-hooks');
 
-const auth = require('feathers-authentication').hooks;
+const process = require('./process');
+const globalHooks = require('../../../hooks');
+const populateSender = hooks.populate('sentBy', {
+    service: 'users',
+    field: 'userId'
+});
 
 exports.before = {
   all: [
@@ -16,16 +19,16 @@ exports.before = {
   find: [],
   get: [],
   create: [process()],
-  update: [],
-  patch: [],
+  update: [hooks.remove('sentBy')],
+  patch: [hooks.remove('sentBy')],
   remove: []
 };
 
 exports.after = {
   all: [],
-  find: [],
-  get: [],
-  create: [],
+  find: [populateSender],
+  get: [populateSender],
+  create: [populateSender],
   update: [],
   patch: [],
   remove: []
